66fa4a531f171c7007eb1083ca902623dc9ea142
ronak <ronkasingh2708@gmail.com>
2024-03-22 14:31:55 +0530

    FlatCards Implemented

---
+++ styler02/components/FlatCards.tsx
@@ -0,0 +1,56 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function FlatCards() {
+  return (
+    <View>
+      <Text style={styles.headingText}>FlatCards</Text>
+      <View style = {styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Blue</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+  },
+  card: {
+    height: 100,
+    width: 100,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin : 10,
+  },
+  cardOne: {
+    backgroundColor: '#FF0000',
+  },
+  cardTwo: {
+    backgroundColor: '#00FF00',
+  },
+  cardThree: {
+    backgroundColor: '#0000FF',
+  },
+  container : {
+    flex : 1,
+    flexDirection : 'row',
+    padding : 5,
+  },
+});
+
+/* rfns */
